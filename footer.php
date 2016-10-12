379df844bd393f5f513f6271240b02912a85877c
Braden <braden337@gmail.com>
2016-10-12 21:43:42 +0000

    changed environment

--- footer.php
+++ footer.php
@@ -7,8 +7,10 @@
 </footer>
 
 <?php wp_footer(); ?>
+<?php if ($_SERVER['ENV'] != "production"): ?>
 <script id="__bs_script__">//<![CDATA[
     document.write("<script async src='https://HOST:8081/browser-sync/browser-sync-client.js?v=2.17.2'><\/script>".replace("HOST", location.hostname));
 //]]></script>
+<?php endif; ?>
 </body>
 </html>
